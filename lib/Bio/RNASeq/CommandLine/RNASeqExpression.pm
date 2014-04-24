fb3fe1e1f74b75eb9bcdc123da02df29d858f676
js21 <js21@sanger.ac.uk>
2014-04-24 10:35:21 +0100

    Compliance with the pathogens inf code structure

--- lib/Bio/RNASeq/CommandLine/RNASeqExpression.pm
+++ lib/Bio/RNASeq/CommandLine/RNASeqExpression.pm
@@ -1,43 +1,79 @@
 #!/usr/bin/env perl
 
-package Bio::RNASeq::Main::RNASeqExpression;
+package Bio::RNASeq::CommandLine::RNASeqExpression;
 
 # ABSTRACT: Calculates RNASeq expression values
-# PODNAME: rna_seq_expression
 
 =head1 SYNOPSIS
 
-This script takes in an aligned sequence file (BAM) and a corresponding annotation file (GFF) and creates a spreadsheet with expression values.
+Takes in an aligned sequence file (BAM) and a corresponding annotation file (GFF) and creates a spreadsheet with expression values.
 The BAM must be aligned to the same reference that the annotation refers to.
 
 =cut
 
-use strict;
-use warnings;
-no warnings 'uninitialized';
-use Getopt::Long;
+use Moose;
+use Getopt::Long qw(GetOptionsFromArray);
 use Bio::RNASeq;
 use Bio::RNASeq::CoveragePlot;
 
-my($sequence_file, $annotation_file, $protocol_name, $output_base_filename, $mapping_quality, $no_coverage_plots, $intergenic_regions, $bitwise_flag, $help );
+has 'args' => ( is => 'ro', isa => 'ArrayRef', required => 1 );
+has 'script_name' => ( is => 'ro', isa => 'Str', required => 1 );
+has 'help' => ( is => 'rw', isa => 'Bool', default => 0 );
 
-my $total_mapped_reads_method = 'default';
+has 'sequence_file' => ( is => 'rw', isa => 'Str');
+has 'annotation_file' => ( is => 'rw', isa => 'Str');
+has 'protocol' => ( is => 'rw', isa => 'Str');
+has 'output_base_filename' => ( is => 'rw', isa => 'Str');
+has 'minimum_mapping_quality' => ( is => 'rw', isa => 'Int', default => 1);
+has 'no_coverage_plots' => ( is => 'rw', isa => 'Bool', default => 0);
+has 'intergenic_regions' => ( is => 'rw', isa => 'Bool', default => 0);
+has 'bitwise_flag' => ( is => 'rw', isa => 'Bool', default => 0);
+has 'total_mapped_reads_method' => ( is => 'rw', isa => 'Str', default => 'default');
 
-GetOptions(
-	   's|sequence_file=s'                      => \$sequence_file,
-	   'a|annotation_file=s'                    => \$annotation_file,
-	   'p|protocol=s'                           => \$protocol_name,
-	   'o|output_base_filename=s'               => \$output_base_filename,
-	   'q|minimum_mapping_quality=s'            => \$mapping_quality,
-	   'c|no_coverage_plots'                    => \$no_coverage_plots,
-	   'i|intergenic_regions'                   => \$intergenic_regions,
-	   'b|bitwise_flag'                         => \$bitwise_flag,
-	   't|total_mapped_reads_method:s'          => \$total_mapped_reads_method,
-	   'h|help'                                 => \$help,
+has '_filters' => ( is => 'rw', isa => 'HashRef', lazy => 1, builder => '_build__filters');
+has '_protocols' => ( is => 'rw', isa => 'HashRef', lazy => 1, builder => '_build__protocols');
+has 'expression_results' => (is => 'rw', isa => 'Object');
+
+
+
+sub BUILD {
+
+	my ($self) =@_;
+	
+	my($sequence_file, $annotation_file, $protocol_name, $output_base_filename, $mapping_quality, $no_coverage_plots, $intergenic_regions, $bitwise_flag, $help );
+
+	GetOptions(
+	$self->args,
+	's|sequence_file=s'                      => \$sequence_file,
+	'a|annotation_file=s'                    => \$annotation_file,
+	'p|protocol=s'                           => \$protocol_name,
+	'o|output_base_filename=s'               => \$output_base_filename,
+	'q|minimum_mapping_quality=s'            => \$mapping_quality,
+	'c|no_coverage_plots'                    => \$no_coverage_plots,
+	'i|intergenic_regions'                   => \$intergenic_regions,
+	'b|bitwise_flag'                         => \$bitwise_flag,
+	't|total_mapped_reads_method:s'          => \$total_mapped_reads_method,
+	'h|help'                                 => \$help,
     );
+	
+	$self->sequence_file($sequence_file) if ( defined($sequence_file) );
+	$self->annotation_file($annotation_file) if ( defined($annotation_file) );
+	$self->protocol($protocol) if ( defined($protocol) );
+	$self->output_base_filename($output_base_filename) if ( defined($output_base_filename) );
+	$self->minimum_mapping_quality($minimum_mapping_quality) if ( defined($minimum_mapping_quality) );
+	$self->no_coverage_plots($no_coverage_plots) if ( defined($no_coverage_plots) );
+	$self->intergenic_regions($intergenic_regions) if ( defined($intergenic_regions) );
+	$self->bitwise_flag($bitwise_flag) if ( defined($bitwise_flag) );
+	$self->total_mapped_reads_method($total_mapped_reads_method) if ( defined($total_mapped_reads_method) );
+	$self->output_multifasta_files($output_multifasta_files) if ( defined($output_multifasta_files) );
 
-($sequence_file && $annotation_file && $protocol_name) or die <<USAGE;
+}
 
+sub run {
+	my ($self) = @_;
+	
+	($self->sequence_file && $self->annotation_file && $self->protocol_name) or die <<USAGE;
+	
 Usage: $0
   -s|sequence_file         <aligned BAM file>
   -a|annotation_file       <annotation file (GFF)>
@@ -54,38 +90,49 @@ This script takes in an aligned sequence file (BAM) and a corresponding annotati
 The BAM must be aligned to the same reference that the annotation refers to and must be sorted.
 USAGE
 
-$output_base_filename ||= $sequence_file;
-$mapping_quality ||= 1;
-my %filters = (mapping_quality => $mapping_quality);
-if(defined($bitwise_flag))
-{
-  $filters{bitwise_flag} = $bitwise_flag ;
+	$self->output_base_filename ||= $self->sequence_file;
+
+	
+	my $expression_results = Bio::RNASeq->new(
+	  sequence_filename    => $self->sequence_file,
+	  annotation_filename  => $self->annotation_file,
+	  filters              => $self->_filters,
+	  protocol             => $self->_protocols{$self->protocol_name},
+	  output_base_filename => $self->output_base_filename,
+	  intergenic_regions   => $self->intergenic_regions,
+	  total_mapped_reads_method   => $self->total_mapped_reads_method
+	  );
+	
+	
+	
+	$expression_results->output_spreadsheet();
+	
+	unless($no_coverage_plots)
+	{
+	  Bio::RNASeq::CoveragePlot->new(
+	    filename             => $expression_results->_corrected_sequence_filename,
+	    output_base_filename => $self->output_base_filename,
+	    mapping_quality      => $self->mapping_quality
+	  )->create_plots();
+	}
 }
 
-my %protocols = ( 
-	standard    => "StandardProtocol",
-	strand_specific => "StrandSpecificProtocol"
-);
 
-my $expression_results = Bio::RNASeq->new(
-  sequence_filename    => $sequence_file,
-  annotation_filename  => $annotation_file,
-  filters              => \%filters,
-  protocol             => $protocols{$protocol_name},
-  output_base_filename => $output_base_filename,
-  intergenic_regions   => $intergenic_regions,
-  total_mapped_reads_method   => $total_mapped_reads_method
-  );
+sub _build__filters {
 
+	my ($self) = @_;
+	$self->_filters(mapping_quality => $self->mapping_quality);
+	if(defined($self->bitwise_flag))
+	{
+	  $self->_filters(bitwise_flag => $self->bitwise_flag) ;
+	}
+}
 
+sub _build_protocols {
 
-$expression_results->output_spreadsheet();
-
-unless($no_coverage_plots)
-{
-  Bio::RNASeq::CoveragePlot->new(
-    filename             => $expression_results->_corrected_sequence_filename,
-    output_base_filename => $output_base_filename,
-    mapping_quality      => $mapping_quality
-  )->create_plots();
+	my ($self) = @_;
+	$self->_protocols = ( 
+		standard    => "StandardProtocol",
+		strand_specific => "StrandSpecificProtocol"
+	);
 }
