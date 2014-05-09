1feaa6133643b68b9d7fce62bbb3bf2e3282aefa
js21 <js21@sanger.ac.uk>
2014-05-09 12:04:49 +0100

    Added BitWise tests. Rest of files not needed

--- t/lib/TestHelper.pm
+++ t/lib/TestHelper.pm
@@ -7,16 +7,16 @@ use Data::Dumper;
 sub mock_execute_script_and_check_output {
     my ( $script_name, $scripts_and_expected_files ) = @_;
 
-    #system('touch empty_file');
-    #open OLDOUT, '>&STDOUT';
-    #open OLDERR, '>&STDERR';
+    system('touch empty_file');
+    open OLDOUT, '>&STDOUT';
+    open OLDERR, '>&STDERR';
     eval("use $script_name ;");
     my $returned_values = 0;
     {
-        #local *STDOUT;
-        #open STDOUT, '>/dev/null' or warn "Can't open /dev/null: $!";
-        #local *STDERR;
-        #open STDERR, '>/dev/null' or warn "Can't open /dev/null: $!";
+        local *STDOUT;
+        open STDOUT, '>/dev/null' or warn "Can't open /dev/null: $!";
+        local *STDERR;
+        open STDERR, '>/dev/null' or warn "Can't open /dev/null: $!";
 
         for my $script_parameters ( sort keys %$scripts_and_expected_files ) {
             my $full_script = $script_parameters;
@@ -38,17 +38,17 @@ sub mock_execute_script_and_check_output {
 				
             unlink($actual_output_file_name);
         }
-        #close STDOUT;
-        #close STDERR;
+        close STDOUT;
+        close STDERR;
     }
 
     # Restore stdout.
-    #open STDOUT, '>&OLDOUT' or die "Can't restore stdout: $!";
-    #open STDERR, '>&OLDERR' or die "Can't restore stderr: $!";
+    open STDOUT, '>&OLDOUT' or die "Can't restore stdout: $!";
+    open STDERR, '>&OLDERR' or die "Can't restore stderr: $!";
 
     # Avoid leaks by closing the independent copies.
-    #close OLDOUT or die "Can't close OLDOUT: $!";
-    #close OLDERR or die "Can't close OLDERR: $!";
+    close OLDOUT or die "Can't close OLDOUT: $!";
+    close OLDERR or die "Can't close OLDERR: $!";
     unlink('empty_file');
 
 }
