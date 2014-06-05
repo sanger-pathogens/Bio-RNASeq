1f5cce0ee218a92abaabc6347260a9d3faf2c47a
js21 <js21@sanger.ac.uk>
2014-06-05 14:40:55 +0100

    Name better suited

---
+++ lib/Bio/RNASeq/DeSeq/Parser/SamplesFile.pm
@@ -0,0 +1,29 @@
+package Bio::RNASeq::DeSeq::Parser::SamplesFile;
+
+use Moose;
+use Bio::RNASeq::DeSeq::Validate::SamplesFile;
+
+has 'samples_file' => ( is => 'rw', isa => 'Str', required => 1 );
+
+sub parse {
+
+    my ($self) = @_;
+    if ( -e $self->samples_file ) {
+
+        my $validator =
+          Bio::RNASeq::DeSeq::Validate::SamplesFile->new(
+            samples_file => $self->samples_file );
+
+        $validator->validate_content_set_samples();
+
+        if ( $validator->is_samples_file_valid == 1 ) {
+            return($validator->samples);
+
+        }
+        else {
+			die "Will need to throw a proper exception here but essentially the file is invalid";
+        }
+    }
+}
+
+1;
