3e6b1561667beca1487d0e804a6a4c117eb6f675
js21 <js21@sanger.ac.uk>
2014-06-11 16:31:36 +0100

    More debugging

--- lib/Bio/DeSeq.pm
+++ lib/Bio/DeSeq.pm
@@ -35,39 +35,36 @@ sub write_deseq_input_file {
 
   my ($self) = @_;
 
-  my $dfile = $self->deseq_file;
-  if ( -e $dfile ) {
-    open( my $fh, '>', './' . $self->deseq_file );
+  open( my $fh, '>', './' . $self->deseq_file );
 
-    my $file_content = "gene_id\t";
+  my $file_content = "gene_id\t";
+
+  for my $file ( sort keys $self->samples ) {
+
+    $file_content .= $self->samples->{$file}->{condition}
+      . $self->samples->{$file}->{replicate} . "\t";
+
+  }
+  $file_content =~ s/\t$//;
+  $file_content .= "\n";
+
+  for my $gene ( @{ $self->genes } ) {
+
+    $file_content .= "$gene\t";
 
     for my $file ( sort keys $self->samples ) {
 
-      $file_content .= $self->samples->{$file}->{condition}
-	. $self->samples->{$file}->{replicate} . "\t";
+      $file_content .=
+	$self->samples->{$file}->{read_counts}->{$gene} . "\t";
 
     }
     $file_content =~ s/\t$//;
     $file_content .= "\n";
-
-    for my $gene ( @{ $self->genes } ) {
-
-      $file_content .= "$gene\t";
-
-      for my $file ( sort keys $self->samples ) {
-
-	$file_content .=
-	  $self->samples->{$file}->{read_counts}->{$gene} . "\t";
-
-      }
-      $file_content =~ s/\t$//;
-      $file_content .= "\n";
-    }
-
-    print $fh "$file_content";
-
-    $self->deseq_fh($fh);
   }
+
+  print $fh "$file_content";
+
+  $self->deseq_fh($fh);
 
 }
 
