2c21369718f6fc95e9e512c36130a09a460ff05a
js21 <js21@sanger.ac.uk>
2014-07-15 11:49:39 +0100

    We need to also capture the gene feature

--- lib/Bio/RNASeq/GFF.pm
+++ lib/Bio/RNASeq/GFF.pm
@@ -38,7 +38,7 @@ sub _build_features
   while( my $raw_feature = $self->_gff_parser->next_feature())
   {
       last unless defined($raw_feature); # No more features
-      next if !($raw_feature->primary_tag eq 'CDS' ||   $raw_feature->primary_tag eq 'polypeptide');
+      next if !($raw_feature->primary_tag eq 'CDS' ||   $raw_feature->primary_tag eq 'polypeptide' ||   $raw_feature->primary_tag eq 'gene');
 
       my $feature_object = Bio::RNASeq::Feature->new(raw_feature => $raw_feature);
 
