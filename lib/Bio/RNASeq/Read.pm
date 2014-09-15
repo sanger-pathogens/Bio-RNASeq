74780d1e8b2da952b47e53e76cd4911f506e7649
andrewjpage <andrewjpage@gmail.com>
2014-09-15 12:01:11 +0100

    allow for cigars with multiple gaps and indels

--- lib/Bio/RNASeq/Read.pm
+++ lib/Bio/RNASeq/Read.pm
@@ -21,56 +21,52 @@ use File::Spec;
 use File::Temp qw/ tempfile /;
 use Bio::RNASeq::Exceptions;
 
-has 'alignment_line' => ( is => 'rw', isa => 'Str',      required   => 1 );
-has 'exons'          => ( is => 'rw', isa => 'ArrayRef', required   => 1 );
-has 'gene_strand'    => ( is => 'rw', isa => 'Int',      required   => 1 );
+has 'alignment_line' => ( is => 'rw', isa => 'Str',      required => 1 );
+has 'exons'          => ( is => 'rw', isa => 'ArrayRef', required => 1 );
+has 'gene_strand'    => ( is => 'rw', isa => 'Int',      required => 1 );
 
 #optional filters
-has 'filters'        => ( is => 'rw', isa => 'Maybe[HashRef]'            );
+has 'filters' => ( is => 'rw', isa => 'Maybe[HashRef]' );
 
-has '_read_details'  => ( is => 'rw', isa => 'HashRef',  lazy_build   => 1 );
-has '_read_position' => ( is => 'rw', isa => 'Int',      lazy_build   => 1 );
+has '_read_details'  => ( is => 'rw', isa => 'HashRef', lazy_build => 1 );
+has '_read_position' => ( is => 'rw', isa => 'Int',     lazy_build => 1 );
 
-has 'read_strand'   => ( is => 'rw', isa => 'Int',      lazy_build   => 1 );
-has 'mapped_reads' => ( is => 'rw', isa => 'HashRef',  lazy_build   => 1 );
-has '_base_positions' => ( is => 'rw', isa => 'ArrayRef', lazy   => 1, builder => '_build__base_positions' );
-has '_base_position_interval' => ( is => 'rw', isa => 'Int', default => 2 );
+has 'read_strand'             => ( is => 'rw', isa => 'Int',      lazy_build => 1 );
+has 'mapped_reads'            => ( is => 'rw', isa => 'HashRef',  lazy_build => 1 );
+has '_base_positions'         => ( is => 'rw', isa => 'ArrayRef', lazy       => 1, builder => '_build__base_positions' );
+has '_base_position_interval' => ( is => 'rw', isa => 'Int',      default    => 2 );
 
-sub _build__read_details
-{
-  my ($self) = @_;
-  
-  my($qname, $flag, $chromosome_name, $read_position, $mapq, $cigar, $mrnm, $mpos, $isize, $seq, $qual) = split(/\t/,$self->alignment_line);
-  
-  my $read_details = {
-    mapping_quality => $mapq,
-    cigar           => $cigar,
-    read_position   => $read_position,
-    flag            => $flag,
-    chromosome_name => $chromosome_name,
-  };
-  # hook to allow for reads to be altered for different protocols
-  $self->_process_read_details($read_details);
-  
-  return $read_details;
+sub _build__read_details {
+    my ($self) = @_;
+
+    my ( $qname, $flag, $chromosome_name, $read_position, $mapq, $cigar, $mrnm, $mpos, $isize, $seq, $qual ) = split( /\t/, $self->alignment_line );
+
+    my $read_details = {
+        mapping_quality => $mapq,
+        cigar           => $cigar,
+        read_position   => $read_position,
+        flag            => $flag,
+        chromosome_name => $chromosome_name,
+    };
+
+    # hook to allow for reads to be altered for different protocols
+    $self->_process_read_details($read_details);
+
+    return $read_details;
 }
 
-sub _build_read_strand
-{
-  my ($self) = @_;
-  my $read_strand = $self->_read_details->{flag} & 16 ? -1:1; # $flag bit set to 0 for forward 1 for reverse.
-  return $read_strand;
+sub _build_read_strand {
+    my ($self) = @_;
+    my $read_strand = $self->_read_details->{flag} & 16 ? -1 : 1;    # $flag bit set to 0 for forward 1 for reverse.
+    return $read_strand;
 }
 
-sub _build__read_position
-{
-  my ($self) = @_;
-  $self->_read_details->{read_position};
+sub _build__read_position {
+    my ($self) = @_;
+    $self->_read_details->{read_position};
 }
 
-
-sub _build_mapped_reads
-{
+sub _build_mapped_reads {
     my ($self) = @_;
 
     my %mapped_reads;
@@ -89,7 +85,7 @@ sub _build_mapped_reads
                 else {
                     $mapped_reads{antisense}++;
                 }
-		return \%mapped_reads;
+                return \%mapped_reads;
             }
         }
     }
@@ -97,152 +93,117 @@ sub _build_mapped_reads
     return \%mapped_reads;
 }
 
-sub _process_read_details
-{
-  # hook to allow for reads to be altered for different protocols
+sub _process_read_details {
+
+    # hook to allow for reads to be altered for different protocols
 }
 
-sub _does_read_pass_filters
-{
-  my ($self) = @_;
-  
-  return 1 unless(defined($self->filters));
-  
-  if(defined($self->filters->{mapping_quality}) && ($self->_read_details->{mapping_quality}  <= $self->filters->{mapping_quality}) )
-  {
-     return 0;
-  }
-  
-  if(defined($self->filters->{bitwise_flag}) && ( ($self->_read_details->{flag} & $self->filters->{bitwise_flag})  == 0 ))
-  {
-    return 0;
-  }
-  
-  return 1;
+sub _does_read_pass_filters {
+    my ($self) = @_;
+
+    return 1 unless ( defined( $self->filters ) );
+
+    if ( defined( $self->filters->{mapping_quality} ) && ( $self->_read_details->{mapping_quality} <= $self->filters->{mapping_quality} ) ) {
+        return 0;
+    }
+
+    if ( defined( $self->filters->{bitwise_flag} ) && ( ( $self->_read_details->{flag} & $self->filters->{bitwise_flag} ) == 0 ) ) {
+        return 0;
+    }
+
+    return 1;
 }
 
-sub _simple_positions
-{
-  my ($self,$read_length) = @_;
-  my @bases;
-  my $final_position = $self->_read_position+$read_length;
-  for(my $i = $self->_read_position; $i < $final_position; $i+=($self->_base_position_interval*2))
-  {
-    push(@bases, $i);
-  }
-  return \@bases;
+sub _simple_positions {
+    my ($self) = @_;
+    my @bases;
+    my $position = $self->_read_position;
+    my $cigar    = $self->_read_details->{cigar};
+
+    while ( $cigar !~ /^$/ ) {
+        if ( $cigar =~ /^([0-9]+[MIDN])/ ) {
+            my $cigar_part = $1;
+            if ( $cigar_part =~ /(\d+)M/ ) {
+                my $position_offset = $1;
+                for ( my $i = $position ; $i < $position + $position_offset ; $i += $self->_base_position_interval ) {
+                    push( @bases, $i );
+                }
+                $position += $position_offset;
+            }
+            elsif ( $cigar_part =~ /(\d+)I/ ) {
+                $position -= $1;
+
+            }
+            elsif ( $cigar_part =~ /(\d+)[ND]/ ) {
+                $position += $1;
+            }
+            $cigar =~ s/$cigar_part//;
+        }
+    }
+    return \@bases;
 }
 
+sub _complex_base_positions {
+    my ($self) = @_;
+    my $bam_file_string = $self->_dummy_seq_line() . $self->alignment_line() . "\n";
 
-sub _simple_positions_with_gap
-{
-  my ($self,$first_read_length, $gap_length, $middle_char, $second_read_length) = @_;
-  my @bases;
+    my ( $fh, $filename ) = tempfile();
+    print {$fh} $bam_file_string;
+    close($fh);
 
-  my $first_final_position =   $self->_read_position+$first_read_length;
-  for(my $i = $self->_read_position; $i < $first_final_position; $i+=$self->_base_position_interval)
-  {
-    push(@bases, $i);
-  }
-  
-  my $final_position; 
-  my $second_starting_position;
-  
-  if($middle_char eq 'N')
-  {
-    $final_position = $self->_read_position + $first_read_length +  $gap_length + $second_read_length; 
-    $second_starting_position = $self->_read_position+$first_read_length+$gap_length;
-  }
-  elsif($middle_char eq 'I')
-  {
-    $final_position = $self->_read_position + $first_read_length + $second_read_length + $gap_length; 
-    $second_starting_position = $self->_read_position+$first_read_length;
-  }
-  elsif($middle_char eq 'D')
-  {
-    $final_position = $self->_read_position + $first_read_length +  $second_read_length - $gap_length; 
-    $second_starting_position = $self->_read_position+$first_read_length;
-  }
-  
-  for(my $i = $second_starting_position ; $i < $final_position; $i+=$self->_base_position_interval)
-  {
-    push(@bases, $i);
-  }  
-  return \@bases;
-}
+    my $mpileup_cmd = "samtools view -buhS $filename 2>/dev/null | samtools mpileup - 2>/dev/null | awk '{if (\$5 ~ /[ACGTacgt]/) print \$2};' | xargs";
+    my $output      = '';
 
-sub _complex_base_positions
-{
-  my ($self) = @_;
-  my $bam_file_string = $self->_dummy_seq_line() . $self->alignment_line() . "\n";
- 
-  my ($fh, $filename) = tempfile();
-  print {$fh} $bam_file_string;
-  close($fh);
+    open OLDERR, '>&STDERR';
+    {
+        local *STDERR;
+        open STDERR, '>/dev/null' or warn "Can't open >/dev/null: $!";
+        $output = `$mpileup_cmd`;
 
-  my $mpileup_cmd = "samtools view -buhS $filename 2>/dev/null | samtools mpileup - 2>/dev/null | awk '{if (\$5 ~ /[ACGTacgt]/) print \$2};' | xargs";
-  my $output = '';
+        close STDERR;
+    }
+    open STDERR, '>&OLDERR' or die "Can't restore stderr: $!";
+    close OLDERR or die "Can't close OLDERR: $!";
 
-  open OLDERR, '>&STDERR';
-  {
-    local *STDERR;
-    open STDERR, '>/dev/null' or warn "Can't open >/dev/null: $!"; 
-    $output = `$mpileup_cmd`;
+    my @base_positions = split( /\s+/, $output );
+    unlink($filename) if ( -e $filename );
 
-    close STDERR;
-  }
-  open STDERR, '>&OLDERR' or die "Can't restore stderr: $!";
-  close OLDERR or die "Can't close OLDERR: $!";
-
-  my @base_positions = split(/\s+/, $output);
-  unlink($filename) if(-e $filename);
-  
-  return \@base_positions;
+    return \@base_positions;
 }
 
 sub _build__base_positions {
 
-  my ($self) = @_;
-  
-  if( $self->_read_details->{cigar} =~ /^([\d]+)M$/)
-  {
-    return $self->_simple_positions($1);
-  }
-  elsif( $self->_read_details->{cigar} =~ /^([\d]+)M([\d]+)([IDN])([\d]+)M$/)
-  {
-    return $self->_simple_positions_with_gap($1, $2, $3, $4);
-  }
-  else
-  {
-    return $self->_complex_base_positions();
-  }
+    my ($self) = @_;
+    if ( $self->_read_details->{cigar}  =~ /^([0-9]+[MIDN])+$/ ) {
+        return $self->_simple_positions();
+    }
+    else {
+        return $self->_complex_base_positions();
+    }
 }
-
 
 sub _dummy_seq_line {
 
-  my ($self) = @_;
-  
-  return '@SQ	SN:' . $self->_read_details->{chromosome_name} . '	LN:' . ( $self->_read_details->{read_position} + 1000000 ) . "\n";
+    my ($self) = @_;
+
+    return '@SQ	SN:' . $self->_read_details->{chromosome_name} . '	LN:' . ( $self->_read_details->{read_position} + 1000000 ) . "\n";
 
 }
 
 # class method
-sub _unmark_duplicates
-{
-  my ($self, $flag) = @_;
-  if ($flag & 1024) 
-  {
-    $flag = $flag - 1024;     
-  }
-  return $flag;
+sub _unmark_duplicates {
+    my ( $self, $flag ) = @_;
+    if ( $flag & 1024 ) {
+        $flag = $flag - 1024;
+    }
+    return $flag;
 }
 
-sub _calculate_bitwise_flag
-{
-	# hook for protocols to update the bitwise flag
-	my ($self, $flag) = @_;
-	return $flag;
+sub _calculate_bitwise_flag {
+
+    # hook for protocols to update the bitwise flag
+    my ( $self, $flag ) = @_;
+    return $flag;
 }
 
 no Moose;
