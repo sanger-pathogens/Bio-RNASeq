62eb977be5384ea416b0a311e81ecde858353863
js21 <js21@sanger.ac.uk>
2014-10-01 10:02:14 +0100

    Thows exception is samples_file doesn't exist

--- lib/Bio/RNASeq/DeSeq/Parser/SamplesFile.pm
+++ lib/Bio/RNASeq/DeSeq/Parser/SamplesFile.pm
@@ -1,9 +1,11 @@
 package Bio::RNASeq::DeSeq::Parser::SamplesFile;
 
 use Moose;
+use Bio::RNASeq::Types;
+use Bio::RNASeq::Exceptions;
 use Bio::RNASeq::DeSeq::Validate::SamplesFile;
 
-has 'samples_file' => ( is => 'rw', isa => 'Str', required => 1 );
+has 'samples_file' => ( is => 'rw', isa => 'Bio::RNASeq::File', required => 1 );
 
 has 'samples'   => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
 has 'exit_c' => ( is => 'rw', isa => 'Bool', default => 1 );
@@ -11,8 +13,9 @@ has 'exit_c' => ( is => 'rw', isa => 'Bool', default => 1 );
 sub parse {
 
   my ($self) = @_;
-  if ( -e $self->samples_file ) {
+# if ( -e $self->samples_file ) {
 
+   print "Hello\n";
     my $validator = Bio::RNASeq::DeSeq::Validate::SamplesFile->new( samples_file => $self->samples_file );
     $validator->validate_content_set_samples();
     $self->samples( $validator->samples );
@@ -20,9 +23,9 @@ sub parse {
     unless ( $validator->is_samples_file_valid ) {
       $self->exit_c(0);
     }
-  } else {
-    $self->exit_c(0);
-  }
+ # } else {
+   #$self->exit_c(0);
+ # }
 }
 
 1;
